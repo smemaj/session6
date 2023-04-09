87457eba6e6b688b2aa4afb3befbcd88e822ad50
smemaj18 <smemaj18@epoka.edu.al>
2023-04-09 18:10:35 +0200

    update save and findAll

--- src/main/java/lhind/internship/main/Main.java
+++ src/main/java/lhind/internship/main/Main.java
@@ -1,19 +1,23 @@
 package lhind.internship.main;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import lhind.internship.model.entity.User;
+import lhind.internship.model.enums.UserRole;
+import lhind.internship.service.UserService;
+import lhind.internship.service.impl.UserServiceImpl;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        User user = new User();
+        user.setUsername("User1");
+        user.setPassword("Pass1");
+        user.setRole(UserRole.BASIC_USER);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        UserService userService = new UserServiceImpl();
+
+        userService.loadAllUsers();
     }
 }
--- src/main/java/lhind/internship/repository/impl/UserRepositoryImpl.java
+++ src/main/java/lhind/internship/repository/impl/UserRepositoryImpl.java
@@ -1,12 +1,23 @@
 package lhind.internship.repository.impl;
 
+import lhind.internship.config.EntityManagerConfig;
 import lhind.internship.model.entity.User;
 import lhind.internship.repository.UserRepository;
+import lhind.internship.util.Queries;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepositoryImpl implements UserRepository {
+
+    private final EntityManager entityManager;
+
+    public UserRepositoryImpl() {
+        entityManager = EntityManagerConfig.getEntityManager();
+    }
+
     @Override
     public Optional<User> findById(Integer integer) {
         return Optional.empty();
@@ -14,12 +25,23 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> findAll() {
-        return null;
+        TypedQuery<User> result = entityManager.createQuery(Queries.FIND_ALL_USERS, User.class);
+        return result.getResultList();
     }
 
     @Override
     public void save(User user) {
-
+        entityManager.getTransaction().begin();
+        if (user.getId() != null) {
+            findById(user.getId()).ifPresent(existingUser -> {
+                user.setUsername(user.getUsername());
+                user.setPassword(user.getPassword());
+                user.setRole(user.getRole());
+            });
+        } else {
+            entityManager.persist(user);
+        }
+        entityManager.getTransaction().commit();
     }
 
     @Override
